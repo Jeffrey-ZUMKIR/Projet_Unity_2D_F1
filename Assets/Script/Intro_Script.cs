b7bc27f0f4410921545c1a4cf1fab63c1c6eacf6
Osef1712 <j.zumkir@ludus-academie.com>
2020-03-20 10:51:07 +0100

    Début

--- Assets/Script/Intro_Script.cs
+++ Assets/Script/Intro_Script.cs
@@ -7,7 +7,8 @@ public class Intro_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int nA = 10;
+        Debug.Log("Ma val est : " + nA);
     }
 
     // Update is called once per frame
