4e858e04b3da1cca1e979932a7054b4436a982cf
Osef1712 <j.zumkir@ludus-academie.com>
2020-04-20 11:58:14 +0100

    Continuation du développement de state.

--- Assets/Script/Intro_Script2.cs
+++ Assets/Script/Intro_Script2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Intro_Script2 : MonoBehaviour
 {
@@ -9,7 +10,15 @@ public class Intro_Script2 : MonoBehaviour
     private int nVal;
 
     //Tableau en C# commence à zéro
-    string[] sString = new string[4];
+    //string[] sString = new string[4];
+    //public string[] sString;
+    [SerializeField] string[] sString=default;
+    //SerializeField
+    //[SerializeField] State StartingState;
+
+    //private member
+    State state;
+    Text MainText;
 
     //Lors du chargement de l'instance programme
     void Awake()
@@ -17,21 +26,47 @@ public class Intro_Script2 : MonoBehaviour
         Debug.Log("Awake");
         nVal = 0;
 
-        sString[0] = "Bonjour";
+        /*sString[0] = "Bonjour";
         sString[1] = "Comment allez-vous?";
         sString[2] = "Ca va et toi?";
-        sString[3] = "Très bien";
+        sString[3] = "Très bien";*/
 
     }
     // Start is called before the first frame update
     void Start()
     {
+        //MainText = GetComponentInChildren<Text>();
         
+        //Debug.Log(MainText.name);
+        //MainText.text = "Salut";
+        //state = StartingState;
+
+        state = State.CreateInstance<State>();
+        //Debug.Log("Name state :" + state.name);
+        state.setStateStory(" lobortis augue vitae metus imperdiet, a laoreet turpis suscipit." +
+            " Nunc pharetra condimentum turpis, ut mollis mi convallis in." + " Nulla facilisi. Integer turpis sem, lacinia ut ornare at, egestas nec dolor. " +
+            "Quisque cursus ex vel finibus rutrum. Phasellus semper consectetur commodo. Proin vel lacus nunc. Sed felis");
+
+
+        MainText = GameObject.Find("MainText").GetComponent<Text>();
+
+        if (state != null)
+        {
+            MainText.text = state.GetSateStory();
+        }
+        else
+        {
+            Debug.Log("SerializeField StartingState is not set");
+        }
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //switchIndexTab();
         /*for (int i = 0; i<sString.Length; i++)
         {
             Debug.Log(sString[i]);
@@ -63,27 +98,30 @@ public class Intro_Script2 : MonoBehaviour
 
 
 
-
-
+        //Debug.Log(sString[nVal]);
+        /*MainText.text = sString[nVal];
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            increase();
+            if (nVal >= sString.Length)
+            {
+                nVal = 0;
+            }
+            else if (nVal < sString.Length - 1)
+            {
+                increase();
+            }
         }
-        else
-         if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            decrease();
-        }
-
-        if (nVal >= sString.Length)
-        {
-            nVal = 0;
-        }
-        else if (nVal < 0)
-        {
-            nVal = sString.Length - 1;
-        }
-        Debug.Log(sString[nVal]);
+            if (nVal < 0)
+            {
+                nVal = sString.Length - 1;
+            }
+            else if (nVal > 0)
+            {
+                decrease();
+            }
+        }*/
 
     }
 
@@ -97,5 +135,35 @@ public class Intro_Script2 : MonoBehaviour
     int decrease()
     {
         return nVal--;
+    }
+
+    void switchIndexTab()
+    {
+        MainText.text = sString[nVal];
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (nVal >= sString.Length)
+            {
+                nVal = 0;
+            }
+            else if (nVal < sString.Length - 1)
+            {
+                increase();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (nVal < 0)
+            {
+                nVal = sString.Length - 1;
+            }
+            else if (nVal > 0)
+            {
+                decrease();
+            }
+        }
+
+
+
     }
 }
---
+++ Assets/Script/State.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetSateStory()
+    {
+        return storyText;
+    }
+
+    public void setStateStory(string strStory)
+    {
+        storyText = strStory;
+    }
+}
